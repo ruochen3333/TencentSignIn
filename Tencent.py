3c5fe26abffcfbc4622e33f725fa63c84609e285
ruochen3333 <85679564+ruochen3333@users.noreply.github.com>
2021-07-05 14:15:04 +0800

    Update Tencent.py

--- Tencent.py
+++ Tencent.py
@@ -69,22 +69,29 @@ def login():
     driver.find_element_by_class_name('J-username').send_keys(EMAIL)
     driver.find_element_by_class_name('J-password').send_keys(PWD)
     driver.find_element_by_class_name('J-loginBtn').click()
-    html = driver.execute_script("return document.documentElement.outerHTML")
-    print(html)
     try:
-        tip = driver.find_element_by_class_name('J-loginTip').text
-        return None
+        iframe = driver.find_element_by_id('wxFrame')
+        src = iframe.get_attribute("src")
+        print(src)
     except Exception:
+        print('报错')
         pass
+#     html = driver.execute_script("return document.documentElement.outerHTML")
+#     print(html)
+#     try:
+#         tip = driver.find_element_by_class_name('J-loginTip').text
+#         return None
+#     except Exception:
+#         pass
     return driver
         
 def SignIn(driver):
     global TEXT
     global DESP
-    if driver is None:
-        TEXT = '签到失败'
-        DESP = '登录失败，账号或密码错误！'
-        return
+#     if driver is None:
+#         TEXT = '签到失败'
+#         DESP = '登录失败，账号或密码错误！'
+#         return
     try:
         driver.get('https://cloud.tencent.com/act/integralmall?from=14376')
         with open('cookie.txt', 'r') as f:
